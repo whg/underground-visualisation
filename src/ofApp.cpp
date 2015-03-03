ebf7743ab9765f827444039cfecba4e813843bb9
Will Gallia <wgallia@gmail.com>
2015-03-03 14:01:22 +0000

    rename project

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,6 +1,45 @@
 #include "ofApp.h"
 
-
+float* getFileData(const char *filename, long size) {
+    
+    FILE *fp;
+    
+    if((fp=fopen(filename, "rb")) == NULL) {
+        cout << "can't open" << endl;
+        return NULL;
+    }
+    
+    fseek(fp, 0, SEEK_END);
+    long fsize = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    
+    cout << "size = " << fsize/4 << endl;
+    
+    float *f = (float *)calloc(size, sizeof(float));
+    if(f==NULL)
+    {
+        fclose(fp);
+        cout << "can't alloc" << endl;
+        return NULL;
+    }
+    
+    size_t res = fread(f, sizeof(float), size/4, fp);
+    if(res != size/4)
+    {
+        fclose(fp);
+        cout << "no data, size = " << size << ", res = " << res << endl;
+    }
+    
+    
+    for (int i = 0; i < 4 * 3; i++) {
+        cout << f[i] << ", ";
+    }
+    cout << endl;
+    
+    fclose(fp);
+    
+    return f;
+}
 
 void ofApp::setup(){
 
